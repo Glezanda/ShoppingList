7335fabc090c7b2f1569666700048af17bef572c
Julia Melik <104920417+Glezanda@users.noreply.github.com>
2024-04-01 10:58:25 +0200

    sdfgh

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { AuthProvider } from './AuthContext'; // Import the AuthProvider
+import reportWebVitals from './reportWebVitals';
 
-// Use createRoot to render your app
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <AuthProvider> {/* Wrap your App component with the AuthProvider */}
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
