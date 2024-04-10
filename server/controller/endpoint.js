8aa9591accdead41804fc15f858a1fb8efb9b1ba
Nooarchon <julia.melik@gmail.com>
2024-04-10 08:19:01 +0200

    change

--- server/controller/endpoint.js
+++ server/controller/endpoint.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const Createdao = require("../dao/create-dao");
-const Getdao = require("../dao/get-dao");
-const Updatedao = require("../dao/update-dao");
-const Deletedao = require("../dao/delete-dao");
-const Listdao = require("../dao/list-dao");
+const Createabl = require("../abl/create-point");
+const Getabl = require("../abl/get-point");
+const Updateabl = require("../abl/update-point");
+const Deleteabl = require("../abl/delete-point");
+const Listabl = require("../abl/list-point");
 
 router.post("/create", async (req, res) => {
-  await Createdao(req, res);
+  await Createabl(req, res);
 });
 
 router.get("/get", async (req, res) => {
-  await Getdao(req, res);
+  await Getabl(req, res);
 });
 
 router.post("/update", async (req, res) => {
-  await Updatedao(req, res);
+  await Updateabl(req, res);
 });
 
 router.post("/delete", async (req, res) => {
-  await Deletedao(req, res);
+  await Deleteabl(req, res);
 });
 
 router.get("/list", async (req, res) => {
-  await Listdao(req, res);
+  await Listabl(req, res);
 });
 
 module.exports = router;
